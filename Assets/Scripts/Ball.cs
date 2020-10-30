20568fdb464cf0afac0369131fc8fc6e2eadc95b
DanielPri <privorotskyd@gmail.com>
2020-10-29 23:28:37 -0400

    [#5] ball now bounces

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float bounceForce = 1;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            rb.AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
+        }
+    }
+}
