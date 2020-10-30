08ba789bbbe83775514adfc197b3d63917f0e159
DanielPri <privorotskyd@gmail.com>
2020-10-30 13:47:16 -0400

    [#3] Refactored input control to Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Structure;
+    public float rotationSpeed = 1;
+
+    private float touchStart = 0;
+    private float displacement;
+    private float prevRotation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevRotation = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleTouch();
+    }
+
+    void HandleTouch()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                DragStart(touch);
+            }
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Dragging(touch);
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                DragRelease(touch);
+            }
+        }
+    }
+
+    void DragStart(Touch touch)
+    {
+        touchStart = touch.position.x;
+    }
+    void Dragging(Touch touch)
+    {
+        displacement = (touch.position.x - touchStart) * rotationSpeed;
+        Structure.transform.eulerAngles = new Vector3(0, prevRotation - displacement, 0);
+    }
+    void DragRelease(Touch touch)
+    {
+        //remember the rotation of the structure for next drag
+        prevRotation = Structure.transform.rotation.eulerAngles.y;
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
