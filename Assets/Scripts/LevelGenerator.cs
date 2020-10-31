7b8bdf532c16aeec2ad8d51d8f67309e05bb1e5f
DanielPri <privorotskyd@gmail.com>
2020-10-31 13:41:30 -0400

    [#9] Infinitely adds platforms using queue

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,41 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
     public GameObject Structure;
     public List<GameObject> RingPrefabs;
+    public Transform Camera;
     public int setupQuantity = 8;
     public float ringOffset = 5;
+
+    private Queue<GameObject> ActiveRings;
     // Start is called before the first frame update
 
     private Vector3 prevRingPosition = Vector3.zero;
 
     void Start()
     {
+        ActiveRings = new Queue<GameObject>();
+
         SetupLevel();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleNextRing();
+    }
+
+    private void HandleNextRing()
+    {
+        if(ActiveRings.Peek().transform.position.y - Camera.position.y > 1.5)
+        {
+            //TODO consider using object pooling
+            Destroy(ActiveRings.Dequeue());
+            AddRingToStructure();
+        }
     }
 
     void SetupLevel()
     {
         for(int i=0; i < setupQuantity; i++)
         {
-            print("Adding ring!");
-            GameObject ring = GetRandomRing();
-            prevRingPosition = ring.transform.position = new Vector3(0, prevRingPosition.y - ringOffset, 0);
-            ring.transform.rotation = Quaternion.Euler(0,Random.Range(0f,360f),0);
+            AddRingToStructure();
         }
     }
 
-    private GameObject GetRandomRing()
+    private void AddRingToStructure()
     {
-        return Instantiate(RingPrefabs[Random.Range(0, RingPrefabs.Count)], Structure.transform, false);
+        GameObject ring = Instantiate(RingPrefabs[Random.Range(0, RingPrefabs.Count)], Structure.transform, false);
+        ActiveRings.Enqueue(ring);
+
+        prevRingPosition = ring.transform.position = new Vector3(0, prevRingPosition.y - ringOffset, 0);
+        ring.transform.rotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
     }
 }
