0628013e524a1769e15e20b10ca105fa12cee85c
DanielPri <privorotskyd@gmail.com>
2020-11-03 19:30:36 -0500

    [#10] menu is prettier

---
+++ Assets/Scripts/MenuStructure.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuStructure : MonoBehaviour
+{
+    public float rotationSpeed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
