985caff62be614dc2325a6bc096feb8be64fb349
DanielPri <privorotskyd@gmail.com>
2020-11-06 16:56:22 -0500

    [#13] much improved particles

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,48 +5,121 @@ public class Ball : MonoBehaviour
 {
     public GameObject particles;
 
+    [Header("Materials")]
+    public Material normalMaterial;
+    public Material fastMaterial;
+
     public float bounceForce = 1;
     
     private Rigidbody rb;
-    
+    private Renderer rendr;
+    private TrailRenderer tr;
+
+    //Trail sizes
+    private AnimationCurve smallCurve;
+    private AnimationCurve bigCurve;
+
+    //Trail colors
+    private Gradient slowTrailGradient;
+    private Gradient fastTrailGradient;
+
+
     [HideInInspector]
     public Action<bool> OnEndingReached;
     private int holesInArow = 0;
     // Start is called before the first frame update
     void Start()
     {
+        rendr = GetComponent<Renderer>();
         rb = GetComponent<Rigidbody>();
+        tr = transform.GetChild(0).GetComponent<TrailRenderer>();
+        smallCurve = tr.widthCurve;
+        bigCurve = InitializeBigCurve();
+        slowTrailGradient = tr.colorGradient;
+        fastTrailGradient = InitializeTrailGradient();
+    }
+
+    private Gradient InitializeTrailGradient()
+    {
+        Gradient gradient = new Gradient();
+
+        gradient.SetKeys(
+            new GradientColorKey[] {new GradientColorKey(Color.red,0f), new GradientColorKey(Color.red, 1.0f)},
+            new GradientAlphaKey[] {new GradientAlphaKey(0.9f, 0f), new GradientAlphaKey(0.5f, 1f) }
+        );
+
+        return gradient;
+    }
+
+    private AnimationCurve InitializeBigCurve()
+    {
+        AnimationCurve curve = new AnimationCurve();
+        curve.AddKey(0f, 0.8f);
+        curve.AddKey(0.93f, 0.08f);
+        return curve;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(holesInArow < 3)
+        if(collision.gameObject.tag == "Floor")
         {
-            if(collision.gameObject.tag == "Floor")
+            if(holesInArow < 3)
             {
                 rb.AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
                 holesInArow = 0;
                 PaintSplatter(collision.GetContact(0).point, collision.gameObject.transform);
             }
+            else
+            {
+                BreakThrough(collision);
+            }
         }
         if (collision.gameObject.tag == "Death")
         {
-            print("Game Over!");
-            OnEndingReached?.Invoke(false);
+            if(holesInArow >= 3)
+            {
+                BreakThrough(collision);
+            }
+            else
+            {
+                print("Game Over!");
+                OnEndingReached?.Invoke(false);
+            }
         }
         else if(collision.gameObject.tag == "Finish")
         {
             print("Victory!");
             OnEndingReached?.Invoke(true);
         }
-        
     }
 
-    private void PaintSplatter(Vector3 pos, Transform parent)
+    private void BreakThrough(Collision col)
+    {
+        col.transform.parent.gameObject.SetActive(false);
+        holesInArow = 0;
+        rendr.material = normalMaterial;
+        tr.time = 0.3f;
+        tr.widthCurve = smallCurve;
+        tr.colorGradient = slowTrailGradient;
+        BigPaintSplatter(col.GetContact(0).point, col.gameObject.transform);
+    }
+
+    private GameObject PaintSplatter(Vector3 pos, Transform parent)
     {
         GameObject splatter = Instantiate(particles, pos, Quaternion.identity, parent);
         splatter.GetComponent<ParticleSystem>().Play();
         Destroy(splatter, 4);
+        return splatter;
+    }
+
+    private void BigPaintSplatter(Vector3 point, Transform transform)
+    {
+        ParticleSystem splatter = PaintSplatter(point, null).GetComponent<ParticleSystem>();
+        var main = splatter.main;
+        main.startColor = Color.red;
+        splatter.emission.SetBurst(0, new ParticleSystem.Burst(0f, 40, 60));
+        var shape = splatter.shape;
+        shape.shapeType = ParticleSystemShapeType.Sphere;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -55,12 +128,13 @@ public class Ball : MonoBehaviour
         {
             other.transform.parent.gameObject.SetActive(false);
             holesInArow++;
-        }
-        else if(holesInArow >= 3 && other.tag == "BreakZone")
-        {
-            other.transform.parent.gameObject.SetActive(false);
-            holesInArow = 0;
-        }
-        
+            if(holesInArow == 3)
+            {
+                rendr.material = fastMaterial;
+                tr.time = 0.5f;
+                tr.widthCurve = bigCurve;
+                tr.colorGradient = fastTrailGradient;
+            }
+        }        
     }
 }
